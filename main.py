47ea218568b761281b20c32aea68bcf90439de3f
trailtony <still_running_uphill@protonmail.com>
2025-10-28 23:10:18 +0000

    Added middlewear server inngest to our Fastapi connection

--- main.py
+++ main.py
@@ -11,3 +11,20 @@ import datetime
 load_dotenv()
 
 
+inngest_client = inngest.Inngest(
+    app_id="rag_app",
+    logger=logging.getLogger("uvicorn"),
+    is_production=False,
+    serializer=inngest.PydanticSerializer()
+)
+
+@inngest_client.create_function(
+    fn_id="RAG: Ingest PDF",
+    trigger=inngest.TriggerEvent(event="rag/ingest_pdf"),
+)
+async def rag_ingest_pdf(ctx: inngest.Context):
+    return {"hello": "world"}
+
+app = FastAPI()
+
+inngest.fast_api.serve(app, inngest_client, [rag_ingest_pdf])
